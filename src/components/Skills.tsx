d7f7e23016f8f07607ff9f6d624091d0b1a4dcd4
bahadurCorvit <fawad301bahadur@gmail.com>
2025-07-26 22:31:10 +0500

    error free

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ import PyTorch from "../assets/pytorch.png";
 import Sklearn from "../assets/sklearn.png";
 import Git from "../assets/git.png";
 import VSCode from "../assets/vscode.png";
-import Jupyter from "../assets/ModelArts.jpg";
+import Jupyter from "../assets/src/modelsArts.jpg";
 
 const Skills = () => {
   return (
